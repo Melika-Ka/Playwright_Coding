80e26b82f692bb2dae1831577da1e65acb2bc1fb
unknown <melika78karimi@gmail.com>
2025-06-30 14:28:38 +0330

    using isChecked -  toBeChecked - toBeFalsy in test

--- tests/ClientApp.spec.js
+++ tests/ClientApp.spec.js
@@ -10,6 +10,17 @@ test("new test", async ({ page }) => {
   await selectBox.selectOption("consult");
   await page.locator("#usertype").last().click();
   await page.locator("#okayBtn").click();
+  await page.locator("#terms").check();
   //
-  await page.pause();
+  // await page.pause();
+
+  /// isChecked and toBeChecked
+  console.log(await page.locator("#terms").isChecked());
+  await expect(page.locator("#terms")).toBeChecked();
+
+  //
+  // await console.log("hi");
+  /// toBeFalsy()
+  await page.locator("#terms").uncheck();
+  await expect(await page.locator("#terms").isChecked()).toBeFalsy();
 });
