f0caacf6a25380284eb8a4c0243202b8cf3d1026
unknown <melika78karimi@gmail.com>
2025-07-16 18:46:10 +0330

    practise code generation with playwrigt

--- tests/testGen.spec.js
+++ tests/testGen.spec.js
@@ -1,11 +1,27 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test('test', async ({ page }) => {
-  await page.goto('https://www.google.com/');
-  await page.getByRole('combobox', { name: 'Search' }).fill('playwright');
-  await page.getByRole('link', { name: 'Playwright: Fast and reliable' }).click();
-  await page.getByRole('link', { name: 'Get started' }).click();
-  await page.getByRole('button', { name: 'Playwright Test' }).click();
-  await page.getByRole('link', { name: 'Library' }).click();
-  await page.getByRole('tab', { name: 'JavaScript' }).first().click();
+test("test", async ({ page }) => {
+  await page.goto("https://demo.playwright.dev/todomvc/#/");
+  await page.getByRole("textbox", { name: "What needs to be done?" }).click();
+  await page
+    .getByRole("textbox", { name: "What needs to be done?" })
+    .fill("hello");
+  await page
+    .getByRole("textbox", { name: "What needs to be done?" })
+    .press("Enter");
+  await page.getByRole("textbox", { name: "What needs to be done?" }).click();
+  await page.getByText("Mark all as complete").click();
+  await page.getByRole("checkbox", { name: "Toggle Todo" }).uncheck();
+  await page.getByRole("textbox", { name: "What needs to be done?" }).click();
+  await page
+    .getByRole("textbox", { name: "What needs to be done?" })
+    .fill("todo");
+  await page
+    .getByRole("textbox", { name: "What needs to be done?" })
+    .press("Enter");
+  await page
+    .getByRole("listitem")
+    .filter({ hasText: "todo" })
+    .getByLabel("Toggle Todo")
+    .check();
 });
